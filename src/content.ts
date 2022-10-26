e0716fd13da71c2c451352fabc48c223ab28c2da
celyes <celyes02@gmail.com>
2022-10-26 15:45:10 +0100

    fix issue in communication

--- src/content.ts
+++ src/content.ts
@@ -1,1 +1,11 @@
-console.log('Hello world!')
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if( request.message === "start" ) {
+            start();
+        }
+    }
+);
+
+function start(){
+    document.body.style.backgroundColor = 'red'
+}
--- src/popup.ts
+++ src/popup.ts
@@ -1,6 +1,8 @@
-// window.addEventListener('DOMContentLoaded', function () {
-const activationButton = window.document.getElementById('activate-reading-mode-btn')!
-activationButton.addEventListener('click', function() {
-    console.log('send message now...')
+window.addEventListener('DOMContentLoaded', function () {
+    const activationButton = document.getElementById('activate-reading-mode-btn')!
+    activationButton.addEventListener('click', function() {
+        chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+            chrome.tabs.sendMessage(tabs[0].id!, {"message": "start"});
+        });
+    })
 })
-// })
